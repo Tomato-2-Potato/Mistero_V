084c122a86e42b82851e31832900811794aeaf35
Tomato-2-Potato <137464103+Tomato-2-Potato@users.noreply.github.com>
2025-03-29 21:36:41 +0530

    Delete basic-calculator.py

--- basic-calculator.py
+++
@@ -1,9 +0,0 @@
-
-n1 = int(input("Enter your first number:"))
-n2 = int(input("Enter your second number: "))
-print("",n1, "raise to the power",n2, "is",n1**n2)
-print(n1,"-",n2, "=", n1-n2)
-print(n1,"+",n2, "=", n1+n2)
-print(n1,"*",n2, "=", n1*n2)
-
-
